c991acad580ea32e77eedd6659c27f20ecae8f2b
JuliFed <fedorchenkojuli@gmail.com>
2018-05-02 16:59:32 +0300

    refactor

--- gilbert.py
+++ gilbert.py
@@ -1,7 +1,7 @@
 from turtle import *
 
 
-def DrawPart(lx, ly):
+def draw_part(lx, ly):
     setpos(X + lx, Y + ly)
     global X
     X = X + lx
@@ -12,44 +12,44 @@ def DrawPart(lx, ly):
 def a(i):
     if i > 0:
         d(i - 1)
-        DrawPart(+lx, 0)
+        draw_part(+lx, 0)
         a(i - 1)
-        DrawPart(0, ly)
+        draw_part(0, ly)
         a(i - 1)
-        DrawPart(-lx, 0)
+        draw_part(-lx, 0)
         c(i - 1)
 
 
 def b(i):
     if i > 0:
         c(i - 1)
-        DrawPart(-lx, 0)
+        draw_part(-lx, 0)
         b(i - 1)
-        DrawPart(0, -ly)
+        draw_part(0, -ly)
         b(i - 1)
-        DrawPart(lx, 0)
+        draw_part(lx, 0)
         d(i - 1)
 
 
 def c(i):
     if i > 0:
         b(i - 1)
-        DrawPart(0, -ly)
+        draw_part(0, -ly)
         c(i - 1)
-        DrawPart(-lx, 0)
+        draw_part(-lx, 0)
         c(i - 1)
-        DrawPart(0, ly)
+        draw_part(0, ly)
         a(i - 1)
 
 
 def d(i):
     if i > 0:
         a(i - 1)
-        DrawPart(0, ly)
+        draw_part(0, ly)
         d(i - 1)
-        DrawPart(lx, 0)
+        draw_part(lx, 0)
         d(i - 1)
-        DrawPart(0, -ly)
+        draw_part(0, -ly)
         b(i - 1)
 
 
