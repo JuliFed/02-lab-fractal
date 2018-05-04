e6d6692f795a4f04a271febb3369c881abf55008
JuliFed <fedorchenkojuli@gmail.com>
2018-05-05 01:29:43 +0300

    CodeStyle homework2

--- hilbert_rewrite.py
+++ hilbert_rewrite.py
@@ -5,78 +5,77 @@
 from turtle import Turtle
 
 
-def draw_part(my_turtle, lx_cor, ly_cor):
+def draw_part(turtle, lx_cor, ly_cor):
     """
     Function draw line to [lx_cor,ly_cor]
     """
-    my_turtle.setpos(my_turtle.xcor()+lx_cor, my_turtle.ycor()+ly_cor)
+    turtle.setpos(turtle.xcor() + lx_cor, turtle.ycor() + ly_cor)
 
 
-def first_line(my_turtle, iter_num, size_line):
+def first_line(turtle, iter_num, line_size):
     if iter_num > 0:
-        fourth_line(my_turtle, iter_num - 1, size_line)
-        draw_part(my_turtle, +size_line, 0)
+        fourth_line(turtle, iter_num - 1, line_size)
+        draw_part(turtle, +line_size, 0)
 
-        first_line(my_turtle, iter_num - 1, size_line)
-        draw_part(my_turtle, 0, size_line)
+        first_line(turtle, iter_num - 1, line_size)
+        draw_part(turtle, 0, line_size)
 
-        first_line(my_turtle, iter_num - 1, size_line)
-        draw_part(my_turtle, -size_line, 0)
+        first_line(turtle, iter_num - 1, line_size)
+        draw_part(turtle, -line_size, 0)
 
-        third_line(my_turtle, iter_num - 1, size_line)
+        third_line(turtle, iter_num - 1, line_size)
 
 
-def second_line(my_turtle, iter_num, size_line):
+def second_line(turtle, iter_num, line_size):
     if iter_num > 0:
-        third_line(my_turtle, iter_num - 1, size_line)
-        draw_part(my_turtle, -size_line, 0)
+        third_line(turtle, iter_num - 1, line_size)
+        draw_part(turtle, -line_size, 0)
 
-        second_line(my_turtle, iter_num - 1, size_line)
-        draw_part(my_turtle, 0, -size_line)
+        second_line(turtle, iter_num - 1, line_size)
+        draw_part(turtle, 0, -line_size)
 
-        second_line(my_turtle, iter_num - 1, size_line)
-        draw_part(my_turtle, size_line, 0)
+        second_line(turtle, iter_num - 1, line_size)
+        draw_part(turtle, line_size, 0)
 
-        fourth_line(my_turtle, iter_num - 1, size_line)
+        fourth_line(turtle, iter_num - 1, line_size)
 
 
-def third_line(my_turtle, iter_num, size_line):
+def third_line(turtle, iter_num, line_size):
     if iter_num > 0:
-        second_line(my_turtle, iter_num - 1, size_line)
-        draw_part(my_turtle, 0, -size_line)
-        third_line(my_turtle, iter_num - 1, size_line)
-        draw_part(my_turtle, -size_line, 0)
-        third_line(my_turtle, iter_num - 1, size_line)
-        draw_part(my_turtle, 0, size_line)
-        first_line(my_turtle, iter_num - 1, size_line)
+        second_line(turtle, iter_num - 1, line_size)
+        draw_part(turtle, 0, -line_size)
+        third_line(turtle, iter_num - 1, line_size)
+        draw_part(turtle, -line_size, 0)
+        third_line(turtle, iter_num - 1, line_size)
+        draw_part(turtle, 0, line_size)
+        first_line(turtle, iter_num - 1, line_size)
 
 
-def fourth_line(my_turtle, iter_num, size_line):
+def fourth_line(turtle, iter_num, line_size):
     if iter_num > 0:
-        first_line(my_turtle, iter_num - 1, size_line)
-        draw_part(my_turtle, 0, size_line)
+        first_line(turtle, iter_num - 1, line_size)
+        draw_part(turtle, 0, line_size)
 
-        fourth_line(my_turtle, iter_num - 1, size_line)
-        draw_part(my_turtle, size_line, 0)
+        fourth_line(turtle, iter_num - 1, line_size)
+        draw_part(turtle, line_size, 0)
 
-        fourth_line(my_turtle, iter_num - 1, size_line)
-        draw_part(my_turtle, 0, -size_line)
+        fourth_line(turtle, iter_num - 1, line_size)
+        draw_part(turtle, 0, -line_size)
 
-        second_line(my_turtle, iter_num - 1, size_line)
+        second_line(turtle, iter_num - 1, line_size)
 
 
-def hilbert(my_turtle, count_iterations, size):
+def hilbert(turtle, count_iterations, size):
     """
         Main function for draw Hilbert curves
     """
-    my_turtle.speed("fast")
-    my_turtle.up()
-    my_turtle.setpos(-100, -100)
-    my_turtle.down()
-    first_line(my_turtle, count_iterations, size)
-    my_turtle.done()
+    turtle.speed("fast")
+    turtle.up()
+    turtle.setpos(-100, -100)
+    turtle.down()
+    first_line(turtle, count_iterations, size)
+    turtle.done()
 
 
 if __name__ == "__main__":
     hilbert(Turtle(), 4, 15)
-
